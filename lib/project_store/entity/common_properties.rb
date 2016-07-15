4703cba4dd068923c9efcc33a25c6a3c7e7142f8
Laurent B <lbnetid+gh@gmail.com>
2016-07-15 15:25:16 +0200

    Removed old code

--- lib/project_store/entity/common_properties.rb
+++ lib/project_store/entity/common_properties.rb
@@ -7,22 +7,6 @@ module ProjectStore
 
       yaml_accessor :description, :data
 
-      # def description
-      #   self[:description]
-      # end
-      #
-      # def description=(description)
-      #   self[:description] = description
-      # end
-      #
-      # def data
-      #   self[:data]
-      # end
-      #
-      # def data=(data)
-      #   self[:data] = data
-      # end
-
     end
 
   end
